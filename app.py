06d63e8b2287c346f5045945ac8dae7683824b10
alaa12004 <alaa.abuhaje@gmail.com>
2025-07-01 00:04:42 +0300

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import google.generativeai as genai
 import os  
 
 app = Flask(__name__)
+CORS(app)
 genai.configure(api_key=os.getenv("API_KEY")) 
 model = genai.GenerativeModel('gemini-pro')
 
